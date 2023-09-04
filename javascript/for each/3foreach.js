2dced94fe5137db0ff0cc9f26b921d11269a5d15
santhosh2826 <santhosh.n.2023@rkmshome.org>
2023-09-04 17:52:23 +0530

    new

---
+++ javascript/for each/3foreach.js
@@ -0,0 +1,6 @@
+const a= [1, 2, 3, 4, 5];
+a.reverse();
+
+a.forEach(i => {
+    console.log(i);
+});
---
+++ javascript/for each/try.js
@@ -0,0 +1,33 @@
+// a=[1,2,3,4,5]
+// for(let i of a)
+// {
+//     console.log(i)
+// }
+
+// a=[1,2,3]
+// function add(e)
+// {
+// console.log(e)
+// }
+// a.forEach(add);
+// a=[1,2,3,4]
+// print=function(e)
+// {
+//     console.log(e)
+// }
+// a.forEach(print)
+// a=[1,2,3]
+// // s=(e)=>{console.log(e)}
+// // a.forEach(s)
+    
+// a.forEach(e => {console.log(e)
+    
+// });
+a=[1,2,3,4,5]
+// //  n=a.map(e=>e*2)
+// // console.log(n)
+// a.forEach(e=>console.log(e))
+n=a.map(function (e){return e})
+console.log(n)
+
+
